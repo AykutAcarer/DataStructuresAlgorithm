30d05b3b158cc0977a683a79e667b6e29f634d12
AykutAcarer <acareraykut@gmail.com>
2023-07-21 22:04:39 +0200

    packages organized

--- src/searchAlgorithms/LinearSearch.java
+++ src/searchAlgorithms/LinearSearch.java
@@ -1,4 +1,4 @@
-package DataStructures;
+package searchAlgorithms;
 
 public class LinearSearch {
 
--- src/sortAlgorithms/InsertionSort.java
+++ src/sortAlgorithms/InsertionSort.java
@@ -1,4 +1,4 @@
-package DataStructures;
+package sortAlgorithms;
 
 public class InsertionSort {
 
